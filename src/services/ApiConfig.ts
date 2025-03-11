fe7d762d24ee36bd649815a5fc6649fef3b2d0e0
Igor Emanuel <igor17emanuel@gmail.com>
2025-03-11 14:19:11 -0300

    First commit

---
+++ src/services/ApiConfig.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/',
+})
+
+const endPoints = {
+  series: '3/tv/changes',
+  movies: '3/discover/movie',
+}
+
+const apiKey = '212e8883e663280ace84f6021868265c'
+
+export { api, endPoints, apiKey }
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import PopularComponent from '@/components/movies/PopularComponent.vue'
+</script>
+
+<template>
+  <h1>Hello Word!</h1>
+  <PopularComponent />
+</template>
